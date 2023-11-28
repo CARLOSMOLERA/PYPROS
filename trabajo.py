3dfdea22b48a2fbdc4cbd8f69d276bfddfee185c
paatriiperezz <152264650+paatriiperezz@users.noreply.github.com>
2023-11-28 09:47:41 +0100

    Create trabajo.py

---
+++ trabajo.py
@@ -0,0 +1,46 @@
+#TRABAJO FINAL
+
+#Primero vamos a poner todas las funciones
+import json 
+
+print('********************************\n*      LIBRERÍA BATOJA         * \n*                              *\n********************************' )
+
+
+def menu():
+    print('\n····MENÚ····')
+    print('1. Agregar libro')
+    print('2. Buscar por título')
+    print('3. Buscar por autor')
+    print('4. Buscar por género')
+    print('5. Revisar estado de préstamo')
+    print('6. Mostrar estadística')
+    print('
+    
+
+def agregar_libro():
+
+def buscar_titulo():
+    
+def buscar_autor():
+
+def buscar_genero():
+    
+def revisar_prestamo():
+    
+def most_estadistica():
+    
+def prestar():
+    
+def devolver():
+    
+def most_todos():
+    
+def most_prestados():
+    
+def modificar():
+    
+def eliminar():
+    
+def most_mejores():
+    
+def salir():
